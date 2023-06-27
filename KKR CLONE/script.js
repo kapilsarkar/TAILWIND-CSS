b81a5045b4fdd1dc26520969e9979235db18165d
kapilsarkar <k.sarkar81@gmail.com>
2023-06-27 19:24:42 +0530

    somesectionsadded

--- KKR CLONE/script.js
+++ KKR CLONE/script.js
@@ -16,3 +16,38 @@ var swiper = new Swiper(".mySwiper", {
       prevEl: ".swiper-button-prev",
     },
   });
+
+  // Team slider ---
+
+ 
+
+  var swiper2 = new Swiper(".mySwiper2", {
+    slidesPerView: 1,
+    spaceBetween: 10,
+    //  autoplay: {
+    //   delay: 1500,
+    //   disableOnInteraction: false,
+    // },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+      640: {
+        slidesPerView: 2,
+        spaceBetween: 20,
+      },
+      768: {
+        slidesPerView: 4,
+        spaceBetween: 40,
+      },
+      1024: {
+        slidesPerView: 5,
+        spaceBetween: 50,
+      },
+    },
+  });
