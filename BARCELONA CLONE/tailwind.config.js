f1af69646a475d49e99b0ea006d9cb2ea231556f
kapilsarkar <k.sarkar81@gmail.com>
2023-07-10 17:42:13 +0530

    headersectiondone

--- BARCELONA CLONE/tailwind.config.js
+++ BARCELONA CLONE/tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["./index.html"],
   theme: {
-    extend: {},
+    extend: {
+      screens:{
+        'media980': '980px',
+       
+      },
+    },
   },
   plugins: [],
 }
