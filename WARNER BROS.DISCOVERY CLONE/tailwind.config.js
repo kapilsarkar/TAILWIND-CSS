126054f6480d20720bde8ae8362ebd2f99346743
kapilsarkar <k.sarkar81@gmail.com>
2023-07-21 06:56:35 +0530

    initialcommit

---
+++ WARNER BROS.DISCOVERY CLONE/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {
+      screens:{
+        'media1220': '1220px',
+        'media360': '360px',
+       
+      },
+    },
+  },
+  plugins: [],
+}
+
