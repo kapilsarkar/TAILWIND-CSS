ccbdea30e474ebaab24fc77bf3e806cbe4a40baa
kapilsarkar <k.sarkar81@gmail.com>
2023-07-22 13:00:20 +0530

    somesectionsadded

--- WARNER BROS.DISCOVERY CLONE/tailwind.config.js
+++ WARNER BROS.DISCOVERY CLONE/tailwind.config.js
@@ -1,15 +1,28 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./index.html"],
+  mode: 'jit',
   theme: {
     extend: {
+      animation: {
+        marquee: 'marquee 15s linear infinite',
+       
+      },
+      keyframes: {
+        marquee: {
+          '0%': { transform: 'translateX(0%)' },
+          '100%': { transform: 'translateX(-100%)' },
+        },
+      },
       screens:{
         'media1220': '1220px',
         'media360': '360px',
         'media1072': '1072px',
+        'media840': '840px',
       },
     },
   },
   plugins: [],
+  
 }
 
